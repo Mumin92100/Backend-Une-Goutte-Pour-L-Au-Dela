563f5fd4810a62e806b1932112b2fc2728383f4e
Mumin92100 <califeryan@gmail.com>
2026-02-17 21:48:18 +0100

    Change MongoDB URL and modify cookie options
    Updated MongoDB connection URL and adjusted cookie settings for session management.

--- index.js
+++ index.js
@@ -47,11 +47,13 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   store: MongoStore.create({
-    mongoUrl: 'mongodb+srv://califeryan_db_user:DZqeO797brr9G5OF@cluster0.j5ezvv2.mongodb.net/ramadan-project'
+    mongoUrl: 'mongodb+srv://califeryan_db_user:DZqeO797brr9G5OF@cluster0.j5ezvv2.mongodb.net/local_ramadan-project'
   }),
   cookie: {
-    sameSite: 'none',
-    secure: true,
+    httpOnly: true,
+    sameSite: 'lax',
+    secure: false, // Mettre à true en production avec HTTPS
+    domain: "axz.onrender.com", // Doit correspondre au domaine de votre application
     maxAge: 24 * 60 * 60 * 1000 // 1 jour
   }
 }))
