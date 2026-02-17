be59ac22668d2df4cd2679fb2ac076e817dd2a0a
Mumin92100 <califeryan@gmail.com>
2026-02-18 00:02:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ const ADMIN_TOKEN = config.adminToken
 const app = express()
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://une-goutte-pour-l-au-dela.vercel.app');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Credentials');
+    res.setHeader('Access-Control-Allow-Origin', 'https://une-goutte-pour-l-au-dela.onrender.com')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Credentials')
     if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
+      return res.sendStatus(204)
     }
     next();
   });
