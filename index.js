bf09f66e08c7c8fd8e79f237cdef8b1484754a5a
Mumin92100 <califeryan@gmail.com>
2026-02-18 00:07:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ const ADMIN_TOKEN = config.adminToken
 
 const app = express()
 app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://une-goutte-pour-l-au-dela.onrender.com')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Credentials')
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204)
-    }
-    next();
-  });
+app.use(cors({
+  origin: [
+    "https://une-goutte-pour-l-au-dela.onrender.com/",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Credentials"],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}))
 
 // Middleware pour parser les données du formulaire
 app.use(express.urlencoded({ extended: true }));
